bec3efb480222f70a455cd9ab04ea16cd5224100
Doğukan İNCİ <dogukaninci93@gmail.com>
2022-05-17 13:53:20 +0300

    Initial Commit

---
+++ Otsimo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Otsimo
+//
+//  Created by Doğukan Inci on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
